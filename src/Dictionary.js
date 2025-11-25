47da6976ab3e8c3e7f12ae1207312718c6b88a09
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2025-11-25 16:30:52 +0100

    updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return 
+    return <div className="Dictionary">
+        <from>
+            <input type="search" placeholder="Type a word..." />    
+        </from>
+    </div>;
 }
