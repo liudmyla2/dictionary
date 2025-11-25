7fe71fd380fee69c3d839d4e37c4694074e65e8e
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2025-11-25 18:25:52 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary App</h1>
         </header>
         <main>
           <Dictionary />
