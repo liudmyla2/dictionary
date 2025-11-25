24c278833907e610811f7007e1825c9803c6e80d
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2025-11-25 12:18:38 +0100

    updated and created new file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="conrainer">
       <header className="App-header">
         <h1>Dictionary App</h1>
-      </header>
+        </header>
+        <main>
+          <Dictionary/>
+        </main>
+        <footer className="text-center">
+         </footer>
+      </div>
     </div>
   );
 }
