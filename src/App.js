08169053181994d482878307037040828e3eaae8
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2025-11-26 10:19:47 +0100

    Added default keaword

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
           <h1>Dictionary App</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by Liudmyla Kostiuk</small>
